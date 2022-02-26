14be81f03dcb9729e564dc3e7092f90472fac4a2
Christopher <chrisabbod@gmail.com>
2022-02-26 14:30:59 -0500

    Add FizzBuzz problem

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class FizzBuzz {
+
+    public static void main(String[] args) {
+
+        int userInput;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+
+        userInput = scanner.nextInt();
+
+        fizzBuzz(userInput);
+    }
+
+    public static void fizzBuzz(int num) {
+        for (int i = 1; i <= num; i++) {
+
+            //If 3 & 5 are multiples of the current number
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
