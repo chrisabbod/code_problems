097c5f770430429619ba9744d51d0d79dfa91242
Christopher <chrisabbod@gmail.com>
2022-08-22 23:12:10 -0400

    Solve LonelyInteger problem in Kotlin

--- src/main/kotlin/LonelyInteger.kt
+++ src/main/kotlin/LonelyInteger.kt
@@ -1,6 +1,43 @@
 class LonelyInteger {
-    /**
-     * Find unique integer that occurs only once
-     *
-     */
+
+    fun lonelyinteger(a: Array<Int>): Int {
+
+        for (i in a) {
+            var uniqueNum = 0
+
+            for (j in a) {
+                if (i == j) {
+                    uniqueNum++
+
+                    if (uniqueNum > 1) {
+                        continue
+                    }
+                }
+            }
+
+            if (uniqueNum == 1) {
+                return i
+            }
+
+        }
+
+        for (i in a) {
+            print("$i ")
+        }
+
+        return 0
+    }
+}
+
+fun main() {
+
+    val li = LonelyInteger()
+
+    val n = readLine()!!.trim().toInt()
+
+    val a = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = li.lonelyinteger(a)
+
+    println(result)
 }
