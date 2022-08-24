6e1bddf6e0fec75035f7ce3beab9d3269b5b963a
Christopher <chrisabbod@gmail.com>
2022-08-23 20:42:30 -0400

    Complete missing integer problem and add problem details file

---
+++ src/main/kotlin/MissingInteger.kt
@@ -0,0 +1,41 @@
+class MissingInteger {
+
+    fun solution(A: IntArray): Int {
+
+        var numToReturn = 1
+        var previousNum: Int
+
+        for (i in A.indices) {
+            previousNum = numToReturn
+
+            for (j in A.indices) {
+                if (numToReturn == A[j]) {
+                    numToReturn++
+                    continue
+                }
+            }
+
+            if (numToReturn == previousNum) {
+                break
+            }
+        }
+
+        return numToReturn
+    }
+}
+
+fun main() {
+
+    val s = MissingInteger()
+    val intArray = IntArray(6)
+
+    intArray[0] = 1
+    intArray[1] = 3
+    intArray[2] = 6
+    intArray[3] = 4
+    intArray[4] = 1
+    intArray[5] = 2
+
+    val smallestInt = s.solution(intArray)
+    println(smallestInt)
+}
