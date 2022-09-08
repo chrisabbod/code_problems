a4cbed01adc778fbbd4f58da78a89076b5dd074a
Christopher <chrisabbod@gmail.com>
2022-09-07 23:00:14 -0400

    Add PlusMinus kotlin solution

---
+++ src/main/kotlin/easy/PlusMinus.kt
@@ -0,0 +1,37 @@
+package easy
+
+class PlusMinus {
+
+    fun plusMinus(arr: Array<Int>) {
+
+        var positive = 0.0
+        var negative = 0.0
+        var zero = 0.0
+
+        arr.forEach {
+            if (it > 0) {
+                positive++
+            } else if (it < 0) {
+                negative++
+            } else {
+                zero++
+            }
+        }
+        
+        println("%,.6f".format(positive / arr.size))
+        println("%,.6f".format(negative / arr.size))
+        println("%,.6f".format(zero / arr.size))
+
+    }
+
+}
+
+fun main() {
+    val pm = PlusMinus()
+
+    val n = readLine()!!.trim().toInt()
+
+    val arr = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    pm.plusMinus(arr)
+}
