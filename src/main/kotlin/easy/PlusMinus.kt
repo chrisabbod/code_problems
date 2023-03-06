0c28f40d4074f160f2bfdaf26c1c7cb44e087aac
Christopher <chrisabbod@gmail.com>
2023-03-05 22:45:33 -0500

    Add optimized solution for plusMinus

--- src/main/kotlin/easy/PlusMinus.kt
+++ src/main/kotlin/easy/PlusMinus.kt
@@ -1,37 +1,17 @@
 package easy
 
 class PlusMinus {
-
-    fun plusMinus(arr: Array<Int>) {
-
-        var positive = 0.0
-        var negative = 0.0
-        var zero = 0.0
-
-        arr.forEach {
-            if (it > 0) {
-                positive++
-            } else if (it < 0) {
-                negative++
-            } else {
-                zero++
-            }
+    companion object {
+        fun plusMinus(arr: Array<Int>) {
+            println(arr.filter { it > 0 }.size / arr.size.toDouble())
+            println(arr.filter { it < 0 }.size / arr.size.toDouble())
+            println(arr.filter { it == 0 }.size / arr.size.toDouble())
         }
-
-        println("%,.6f".format(positive / arr.size))
-        println("%,.6f".format(negative / arr.size))
-        println("%,.6f".format(zero / arr.size))
-
     }
-
 }
 
 fun main() {
-    val pm = PlusMinus()
-
     val n = readLine()!!.trim().toInt()
-
     val arr = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
-
-    pm.plusMinus(arr)
+    PlusMinus.plusMinus(arr)
 }
