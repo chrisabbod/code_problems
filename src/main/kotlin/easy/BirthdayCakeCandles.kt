380c519c2fc50f8352d6a806714a0b336515fe97
Christopher <chrisabbod@gmail.com>
2023-03-06 22:30:41 -0500

    Solve BirthdayCakeCandles problem

--- src/main/kotlin/easy/BirthdayCakeCandles.kt
+++ src/main/kotlin/easy/BirthdayCakeCandles.kt
@@ -1,4 +1,18 @@
 package easy
 
 class BirthdayCakeCandles {
+    companion object {
+        fun birthdayCakeCandles(candles: Array<Int>): Int {
+            val tallest = candles.max()!!
+            return candles.count { it == tallest }
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    val candlesCount = readLine()!!.trim().toInt()
+    val candles = readLine()!!.trimEnd().split(" ").map { it.toInt() }.toTypedArray()
+    val result = BirthdayCakeCandles.birthdayCakeCandles(candles)
+
+    println(result)
 }
