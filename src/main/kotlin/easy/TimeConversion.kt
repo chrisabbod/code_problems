98e1d7f0c54eb19616fb6ffbde2cd37daa76ac73
Christopher <chrisabbod@gmail.com>
2023-03-06 22:34:24 -0500

    Put TimeConversion problem into it's own class file

--- src/main/kotlin/easy/TimeConversion.kt
+++ src/main/kotlin/easy/TimeConversion.kt
@@ -1,4 +1,24 @@
 package easy
 
 class TimeConversion {
+    companion object {
+        fun timeConversion(s: String): String {
+            val splitString = s.split(":")
+            var hour = splitString[0].toInt()
+            val minute = splitString[1]
+            val second = splitString[2].substring(0, 2)
+            val amPm = splitString[2].substring(2, 4)
+
+            if (amPm == "AM" && hour == 12) { hour = 0 }
+            if (amPm == "PM" && hour != 12) { hour += 12 }
+
+            return if (hour < 10) { "0$hour:$minute:$second"} else { "$hour:$minute:$second" }
+        }
+    }
+}
+
+fun main() {
+    val s = readLine()!!
+    val result = TimeConversion.timeConversion(s)
+    println(result)
 }
